8294a5592c5d8dd3e3f5ee9ddf178341021e89fd
Junu Razeen <junaid.abrar.razeen@gmail.com>
2023-08-21 01:03:48 +0600

    components and pages

---
+++ client/src/pages/Home.js
@@ -0,0 +1,114 @@
+import React , {useState,useEffect} from 'react'
+import { useSelector , useDispatch } from 'react-redux'
+import DefaultLayout from '../components/DefaultLayout'
+import { getAllCars } from '../redux/actions/carsActions'
+import { Col, Row , Divider , DatePicker, Checkbox} from 'antd'
+import {Link} from 'react-router-dom'
+import Spinner from '../components/Spinner';
+import moment from 'moment'
+const {RangePicker} = DatePicker
+function Home() {
+    const {cars} = useSelector(state=>state.carsReducer)
+    const {loading} = useSelector(state=>state.alertsReducer)
+    const [totalCars , setTotalcars] = useState([])
+    const dispatch = useDispatch()
+    
+
+    useEffect(() => {
+        dispatch(getAllCars())
+    }, [])
+
+    useEffect(() => {
+
+        setTotalcars(cars)
+        
+    }, [cars])
+
+
+    function setFilter(values){
+
+        var selectedFrom = moment(values[0] , 'MMM DD yyyy HH:mm')
+        var selectedTo = moment(values[1] , 'MMM DD yyyy HH:mm')
+
+        var temp=[]
+
+        for(var car of cars){
+
+              if(car.bookedTimeSlots.length == 0){
+                  temp.push(car)
+              }
+              else{
+
+                   for(var booking of car.bookedTimeSlots) {
+
+                       if(selectedFrom.isBetween(booking.from , booking.to) ||
+                       selectedTo.isBetween(booking.from , booking.to) || 
+                       moment(booking.from).isBetween(selectedFrom , selectedTo) ||
+                       moment(booking.to).isBetween(selectedFrom , selectedTo)
+                       )
+                       {
+
+                       }
+                       else{
+                           temp.push(car)
+                       }
+
+                   }
+
+              }
+
+        }
+
+
+        setTotalcars(temp)
+
+
+    }
+
+    return (
+        <DefaultLayout>
+
+            
+              {loading == true && (<Spinner/>)}
+
+
+              <Row justify="center" gutter={1} className="mt-1">
+            <Col lg={20} sm={24}>
+            <div className="d-flex justify-content-between align-items-center">
+
+                <button className="btn1">
+                <a href="/addcar">ADD CAR</a>
+                </button>
+            </div>
+            </Col>
+        </Row>
+              <Row justify='center' gutter={16}>
+
+                   {totalCars.map(car=>{
+                       return <Col lg={5} sm={24} xs={24}>
+                            <div className="car p-2 bs1">
+                               <img src={car.image} className="carimg"/>
+
+                               <div className="car-content d-flex align-items-center justify-content-between">
+
+                                    <div className='text-left pl-2'>
+                                        <p>{car.name}</p>
+                                        <p> Rent Per Hour {car.rentPerHour} BDT</p>
+                                    </div>
+
+                                    <div>
+                                        <button className="btn1 mr-2"><Link to={`/booking/${car._id}`}>Book Now</Link></button>
+                                    </div>
+
+                               </div>
+                            </div>
+                       </Col>
+                   })}
+
+              </Row>
+
+        </DefaultLayout>
+    )
+}
+
+export default Home
