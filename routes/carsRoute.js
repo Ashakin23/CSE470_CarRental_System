e1082ec167d284caa6258f82c1235b7c05c8cdaa
navid001 <navidalvi.001@gmail.com>
2023-08-21 01:15:19 +0600

    adding the router files

---
+++ routes/carsRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const Car = require("../models/carModel");
+
+router.get("/getallcars", async (req, res) => {
+  try {
+    const cars = await Car.find();
+    res.send(cars);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+
+router.post("/addcar", async (req, res) => {
+  try {
+    const newcar = new Car(req.body);
+    await newcar.save();
+    res.send("Car added successfully");
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+
+
+module.exports = router;
