3d2e12a434ec771bf63d25615206fb5b33cdb0ee
Ashakin-sus <kazihabibul2@gmail.com>
2023-08-21 01:14:16 +0600

    models file. works as db schemas

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+     username : {type:String , required: true, unique: true},
+     password : {type:String , required: true},
+     name : {type:String , required: true},
+     dob : {type:String , required: true},
+     nid : {type:String , required: true},
+     contact : {type:String , required: true},
+     email : {type:String , required: true}
+})
+
+const userModel = mongoose.model('users' , userSchema)
+
+module.exports = userModel
